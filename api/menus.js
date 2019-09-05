d6b96edafb8b5835ff4203148d15c569beef5dd1
Unknown <m.kudamatsu@gmail.com>
2019-09-05 14:32:13 +0900

    Handle GET requests to /menus #7

--- api/menus.js
+++ api/menus.js
@@ -4,5 +4,15 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
 
+// GET all requests
+menusRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Menu', (err, rows) => {
+    if (err) {
+      next(err);
+    } else {
+      res.status(200).json({menus: rows});
+    }
+  });
+});
 
 module.exports = menusRouter;
